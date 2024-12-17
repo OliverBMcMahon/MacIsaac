f76cfb65a12c5225a47fb02edf3561db4128e415
Oliver McMahon <mcmahon.oliver@gmail.com>
2024-12-17 06:13:48 -0800

    Add files via upload
    GetMonitorList.py defines a function that returns a list of rows from the monitors table in the database.

---
+++ GetMonitorList.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 12:32:26 2024
+
+@author: oliver
+"""
+
+import sqlite3
+
+def GetMonitors():
+    conn = sqlite3.connect("GymTest2.db")
+    cursor = conn.cursor()
+    cmd = "SELECT * FROM monitors;"
+    cursor.execute(cmd)
+    output = cursor.fetchall() 
+    return output
+
+#GetMonitors()
+  
