b732d0949c9dc7ed5d796d11f38ef70de0816a90
user.name <mcmahon.oliver@gmail.com>
2024-12-17 10:05:09 -0500

    Add sqlite code to store values in db

--- ControlTemperatureAndHumidity.py
+++ ControlTemperatureAndHumidity.py
@@ -5,15 +5,28 @@ from datetime import datetime
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect(('169.254.168.139', 23))
 
+conn = sqlite3.connect("GymTest2.db")
+
+cursor = conn.cursor()
+
 msg = 'hello'
 s.send(msg.encode())
 msg = s.recv(1024)
 print( msg.decode("utf-8"))
 words = msg.decode("utf-8").split(',')
-print(words[0].strip())
-print(words[1].strip())
+
+name = 'monitor1'
+humidity = float(words[0].strip())
+temperature = float(words[1].strip())
 nowstr = datetime.now().strftime('%Y/%m/%d/%H/%M')
 print(nowstr)
+
+cmdstr = "INSERT INTO reports (datetime, monitor, temperature, humidity) VALUES(?, ?, ?, ?);"
+cursor.execute((cmdstr),(nowstr, name, temperature, humidity))
+rows = cursor.fetchall()
+
+conn.commit()
+conn.close()
 
 
     
