8bb8502127c007bc8101dec10bc7ec6889d3f9ce
user.name <mcmahon.oliver@gmail.com>
2024-12-18 08:27:46 -0500

    Convert temperature to F. Add def main function.

--- ControlTemperatureAndHumidity.py
+++ ControlTemperatureAndHumidity.py
@@ -1,12 +1,16 @@
 import socket
 import sqlite3
+import time
 from datetime import datetime
 
 # Define functions that will be used in the code which follows
-# MonitorMessage is the row of data returned from themonitors table
+# MonitorMessage is the row of data returned from the monitors table
 def GetTemperatureFromMonitorMessage(msg):
     words = msg.decode("utf-8").split(',')
-    temperature = float(words[1].strip())
+    temperature = float(words[1].strip()) 
+    print(temperature)
+    temperature = temperature * 1.8 + 32.0
+    print(temperature)
     return temperature
 
 def GetHumidityFromMonitorMessage(msg):
@@ -29,6 +33,8 @@ def GetMonitorReport(name, ip, port):
     nowstr = datetime.now().strftime('%Y/%m/%d/%H/%M')
     print(nowstr)
 
+    conn = sqlite3.connect("GymTest2.db")
+    cursor = conn.cursor()
     cmdstr = "INSERT INTO reports (datetime, monitor, temperature, humidity) VALUES(?, ?, ?, ?);"
     cursor.execute((cmdstr),(nowstr, name, temperature, humidity))
 
@@ -41,33 +47,49 @@ def GetMonitors():
     cmd = "SELECT * FROM monitors;"
     cursor.execute(cmd)
     output = cursor.fetchall() 
+    conn.close()
     return output
 
 def GetMonitorName(monitor_row):
     words = monitor_row.split(',')
     print(words)
 
+def GetReports():
+    # Get list of monitors
+    monitors = GetMonitors()
+    # Extract parameters for monitor1
+    monitor = monitors[0]
+    name = monitor[0]
+    ip = monitor[2]
+    port = monitor[3]
+    # Show monitor name and ip address
+    print(name + " " + ip)
+    GetMonitorReport(name, ip, port)
+    
 # Begin application code ------------------------------------------
+def main():
+    # Setup for timer interval loop
+    intervalMinutes = 5
+    t1 = datetime.now()
+    elapsed = 0
+    while True:
+        t2 = datetime.now()
+        elapsed = t2 - t1
+        if elapsed.seconds/60 >= intervalMinutes:
+            GetReports()
+            t1 = datetime.now()
+        
+if __name__ == '__main__':
+    main()
 
-# Connect to databse 
-conn = sqlite3.connect("GymTest2.db")
-cursor = conn.cursor()
-
-monitors = GetMonitors()
+#monitors = GetMonitors()
 # When we have 2 monitors running we will implement this loop
 #for monitor in monitors:
 #        monitor = monitors[0]
 #        ip = monitor[2]
 #        port = monitor[3]
 
-monitor = monitors[0]
-name = monitor[0]
-ip = monitor[2]
-port = monitor[3]
 
-print(name + " " + ip)
-
-GetMonitorReport(name, ip, port)
 
 
 
