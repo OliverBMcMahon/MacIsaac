5699d6bb078a55253af665379eabdda893dd6800
user.name <mcmahon.oliver@gmail.com>
2024-12-27 16:05:19 -0500

    Modified to get monitor reports from database. Changed logfile prefix to CONTROL.

--- ControlTemperatureAndHumidity.py
+++ ControlTemperatureAndHumidity.py
@@ -1,60 +1,46 @@
 # -*- coding: utf-8 -*-
-import socket
 import sqlite3
-import MyLogger as logger
+import time
 import MonitorClass
-#import RelayControl
+import RelayControl
+import MyLogger as logger
+import MonitorTemperatureAndHumidity
 import CreateLogfilename as clf
 import UtilityFunctions as ufcn
-import EnvironmentReportClass as erc
 from datetime import datetime
 from configparser import ConfigParser
 
 # Global variables
 iniFile = 'MacIsaac.ini'
 
-# Functions 
-def GetMonitorReport(name, ip, port):
-    try:
-        # Create socket to connect to Arduino monitor.
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((ip, port))               
-        hello = 'bah'
-        s.send(hello.encode())    
-        msg = s.recv(1024)
-    
-        humidity = ufcn.GetHumidityFromMonitorMessage(msg)
-        temperature = ufcn.GetTemperatureFromMonitorMessage(msg)
-        
-        rpt = erc.EnvironmentReport(name, temperature, humidity)
-    
-        dbname = ufcn.GetDatabaseName(iniFile)
-        conn = sqlite3.connect(dbname)
-        cursor = conn.cursor()
-        cmdstr = "INSERT INTO reports (datetime, monitor, temperature, humidity) VALUES(?, ?, ?, ?);"
-        cursor.execute((cmdstr),(rpt.rpttime, name, temperature, humidity))
-        conn.commit()
-    except socket.error as err:
-        logger.logErrorMsg(err)
-    finally:
-        conn.close()    
-    return rpt
-    
-
+# Get most recent reports from database.
 def GetReports():
-    envReports = []
+    reports = []
     logger.logInfoMsg("GetReports()")
-    monitors = ufcn.GetMonitors(iniFile)
+    dbname = ufcn.GetDatabaseName(iniFile)
+    monitors = ufcn.GetMonitors(iniFile)   
     for monitor in monitors:
-        newrpt = GetMonitorReport(monitor.name, monitor.ip, monitor.port)
-        logger.logInfoMsg(f"{monitor.name} {monitor.ip} {monitor.port}")
-        envReports.append(newrpt)
-    return envReports
+        try:
+            conn = sqlite3.connect(dbname)
+            cursor = conn.cursor()
+            name = monitor.name
+            cmd = f'''SELECT * FROM reports 
+                WHERE monitor LIKE '{name}'
+                ORDER BY datetime DESC LIMIT 1;'''
+            cursor.execute(cmd)
+            newrpt = cursor.fetchall() 
+            reports.append(newrpt)
+            print(newrpt)
+        except Exception as e:
+            print(e)
+        finally:
+            conn.close()     
+    return reports
     
 # Begin application code ------------------------------------------
 def main():
     # Get unique nasme for logfile.
-    logfile = clf.CreateLogfilename()
+    logfile = clf.CreateLogfilename("CONTROL")
     # Send log filename to logger.
     logger.assignLogfilename(logfile)
     logger.logInfoMsg("Begin new run.")
@@ -71,11 +57,11 @@ def main():
         t2 = datetime.now()
         elapsed = t2 - t1
         if elapsed.seconds/60 >= float(intervalMinutes):
-            envReports = GetReports()
-            # Update Arduino's interval start time.
+            reports = GetReports()
+            # Reset interval start time.
             t1 = datetime.now()
-        # Call SetRelays with envReports.
-        #RelayControl.SetRelays(envReports)        
+            # SetRelays with envReports.
+            RelayControl.SetRelays(reports)        
    
 # Run main() if this file is being run directly       
 if __name__ == '__main__':
